fec913ef07057119b3d59d13d0dfc5b48cdc8218
User Name <maelshaer83@gmail.com>
2021-11-28 18:04:14 +0200

    add 404 page

---
+++ wp-content/themes/greenstor/404.php
@@ -0,0 +1,70 @@
+<?php get_header(); ?>
+<header id="main-header">
+    <div class="container">
+        <h1>نأسف الصفحة غير موجودة يمكنك إعادة البحث <?php echo get_search_query(); ?></h1>
+        <?php get_search_form(); ?>
+    </div>
+</header>
+<!-- Blog posts -->
+<section id="blog-bosts">
+    <h2>تصفح أحدث مقالتنا</h2>
+    <div class="container posts-container">
+        <!-- اعرض احدث اربع مقالات فقط -->
+        <?php $the_query= new WP_Query('posts_per_page=4'); ?>
+        <!-- لو في مقالات اعرضها لو مافيش اظهر رسالة الخطأ -->
+
+        <?php if($the_query -> have_posts()): ?>
+        <?php while($the_query -> have_posts()): ?>
+        <?php $the_query -> the_post(); ?>
+
+        <article class="card">
+            <!-- لاظهار الصور -->
+            <?php if(has_post_thumbnail()): ?>
+            <a href="<?php the_permalink(); ?>">
+                <?php the_post_thumbnail('medium'); ?>
+                <!-- <img src="https://via.placeholder.com/300" alt=""> -->
+            </a>
+            <?php else: ?>
+            <a href="<?php the_permalink(); ?>">
+                <img src="https://via.placeholder.com/300" alt="">
+            </a>
+            <?php endif; ?>
+            <div>
+                <h3>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                    <!-- <a href="#">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing.
+                    </a> -->
+                </h3>
+                <!-- اظهر المقالة -->
+                <p>
+                    <?php the_excerpt(); ?>
+                </p>
+                <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Aliquam ea expedita quas reiciendis
+                    maiores doloribus!
+                </p> -->
+
+                <div class="post-catogeries">
+                    <!-- ديناميك التصنيفات تظهر -->
+                    <?php the_category(' '); ?>
+                    <!-- <a href="#">category1</a>
+                    <a href="#">category2</a>
+                    <a href="#">category3</a> -->
+                </div>
+            </div>
+        </article>
+        <?php
+                endwhile;
+                else:
+                echo "ther are no articales";
+                endif;
+        ?>
+
+    </div>
+    <?php get_template_part('pagination'); ?>
+
+</section>
+<?php get_footer(); ?>
--- wp-content/themes/greenstor/pagination.php
+++ wp-content/themes/greenstor/pagination.php
@@ -6,21 +6,19 @@
     <div class="container">
         <div class="nav-previous align-left">
             <?php if(is_single()): ?>
-            <?php previous_post_link('<strong>السابق - %link</strong>'); ?>
+            <?php previous_post_link('<strong>%link</strong>'); ?>
             <?php else:?>
-            <?php previous_posts_link('Older posts'); ?>
+            <?php previous_posts_link('Previous posts'); ?>
             <?php endif; ?>
-
             <!-- <a href="#">Older posts</a> -->
         </div>
         <div class="nav-next align-right">
             <?php if(is_single()): ?>
-            <?php next_post_link('<strong>التالي - %link</strong>'); ?>
+            <?php next_post_link('<strong>%link</strong>'); ?>
             <?php else:?>
-            <?php next_posts_link('Newer posts'); ?>
+            <?php next_posts_link('Next posts'); ?>
             <?php endif; ?>
             <!-- <a href="#">Newer posts</a> -->
-
         </div>
     </div>
 </div>
